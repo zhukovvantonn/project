ab181cf52509f079d2afd56d14c9e1dd761c650f
Anton <zhukovv.antonn@gmail.com>
2021-01-21 16:08:03 +0200

    first commit

---
+++ js — копия/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('arr' + "- object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    
+    console.log(2*4 == 8);
